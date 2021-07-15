6acee9864af2eba292d13a0bc2fb4d7d199357a3
Wojciech Meller <wmeller@antmicro.com>
2021-07-15 12:17:40 +0200

    [#27438] RiscV: Add load for V-extension

---
+++ arch/riscv/vector_helper_template.h
@@ -0,0 +1,106 @@
+
+#define DATA_SIZE (1 << SHIFT)
+
+#ifdef MASKED
+#define POSTFIX _m
+#else
+#define POSTFIX
+#endif
+
+#if DATA_SIZE == 8
+#define BITS      64
+#define SUFFIX    q
+#define USUFFIX   q
+#define DATA_TYPE uint64_t
+#elif DATA_SIZE == 4
+#define BITS      32
+#define SUFFIX    l
+#define USUFFIX   l
+#define DATA_TYPE uint32_t
+#elif DATA_SIZE == 2
+#define BITS      16
+#define SUFFIX    w
+#define USUFFIX   uw
+#define DATA_TYPE uint16_t
+#elif DATA_SIZE == 1
+#define BITS      8
+#define SUFFIX    b
+#define USUFFIX   ub
+#define DATA_TYPE uint8_t
+#else
+#error unsupported data size
+#endif
+
+void glue(glue(helper_vle, BITS), POSTFIX)(CPUState *env, uint32_t vd, uint32_t rs1, uint32_t lumop, uint32_t nf)
+{
+    target_ulong src_addr = env->gpr[rs1];
+    for (int ei = env->vstart; ei < env->vl; ++ei) {
+#ifdef MASKED
+        if (!(V(0)[ei >> 3] & (1 << (ei & 0x7)))) {
+            continue;
+        }
+#endif
+        for (int fi = 0; fi <= nf; ++fi) {
+            ((DATA_TYPE *)V(vd + fi))[ei] = glue(ld, USUFFIX)(src_addr + ei * DATA_SIZE);
+        }
+    }
+}
+
+void glue(glue(helper_vlse, BITS), POSTFIX)(CPUState *env, uint32_t vd, uint32_t rs1, uint32_t rs2, uint32_t nf)
+{
+    target_ulong src_addr = env->gpr[rs1];
+    target_long offset = env->gpr[rs2];
+    for (int ei = env->vstart; ei < env->vl; ++ei) {
+#ifdef MASKED
+        if (!(V(0)[ei >> 3] & (1 << (ei & 0x7)))) {
+            continue;
+        }
+#endif
+        for (int fi = 0; fi <= nf; ++fi) {
+            ((DATA_TYPE *)V(vd + fi))[ei] = glue(ld, USUFFIX)(src_addr + ei * offset);
+        }
+    }
+}
+
+void glue(glue(helper_vlxei, BITS), POSTFIX)(CPUState *env, uint32_t vd, uint32_t rs1, uint32_t vs2, uint32_t nf)
+{
+    target_ulong src_addr = env->gpr[rs1];
+    DATA_TYPE *offsets = (DATA_TYPE *)V(vs2);
+    const target_ulong dst_eew = env->vsew;
+
+    for (int ei = env->vstart; ei < env->vl; ++ei) {
+#ifdef MASKED
+        if (!(V(0)[ei >> 3] & (1 << (ei & 0x7)))) {
+            continue;
+        }
+#endif
+        for (int fi = 0; fi <= nf; ++fi) {
+            switch (dst_eew) {
+            case 8:
+                V(vd + fi)[ei] = ldub(src_addr + ((DATA_TYPE *)V(vs2 + fi))[ei]);
+                break;
+            case 16:
+                ((uint16_t *)V(vd + fi))[ei] = lduw(src_addr + offsets[ei] + sizeof(DATA_TYPE) * fi);
+                break;
+            case 32:
+                ((uint32_t *)V(vd + fi))[ei] = ldl(src_addr + offsets[ei] + sizeof(DATA_TYPE) * fi);
+                break;
+            case 64: 
+                ((uint64_t *)V(vd + fi))[ei] = ldq(src_addr + offsets[ei] + sizeof(DATA_TYPE) * fi);
+                break;
+            default:
+                tlib_abortf("Unsupported EEW");
+                break;
+            }
+        }
+    }
+}
+
+#undef SHIFT
+#undef DATA_TYPE
+#undef BITS
+#undef SUFFIX
+#undef USUFFIX
+#undef DATA_SIZE
+#undef MASKED
+#undef POSTFIX
