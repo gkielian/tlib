7de3425db97244f42d78804a25d2ebeb3e64f6b5
Wojciech Meller <wmeller@antmicro.com>
2021-07-15 12:18:52 +0200

    [#27438] RiscV: Add store for V-extension

--- arch/riscv/vector_helper_template.h
+++ arch/riscv/vector_helper_template.h
@@ -96,6 +96,71 @@ void glue(glue(helper_vlxei, BITS), POSTFIX)(CPUState *env, uint32_t vd, uint32_
     }
 }
 
+void glue(glue(helper_vse, BITS), POSTFIX)(CPUState *env, uint32_t vd, uint32_t rs1, uint32_t sumop, uint32_t nf)
+{
+    target_ulong src_addr = env->gpr[rs1];
+    for (int ei = env->vstart; ei < env->vl; ++ei) {
+#ifdef MASKED
+        if (!(V(0)[ei >> 3] & (1 << (ei & 0x7)))) {
+            continue;
+        }
+#endif
+        for (int fi = 0; fi <= nf; ++fi) {
+            glue(st, SUFFIX)(src_addr + ei * DATA_SIZE, ((DATA_TYPE *)V(vd + fi))[ei]);
+        }
+    }
+}
+
+void glue(glue(helper_vsse, BITS), POSTFIX)(CPUState *env, uint32_t vd, uint32_t rs1, uint32_t rs2, uint32_t nf)
+{
+    target_ulong src_addr = env->gpr[rs1];
+    target_long offset = env->gpr[rs2];
+    for (int ei = env->vstart; ei < env->vl; ++ei) {
+#ifdef MASKED
+        if (!(V(0)[ei >> 3] & (1 << (ei & 0x7)))) {
+            continue;
+        }
+#endif
+        for (int fi = 0; fi <= nf; ++fi) {
+            glue(st, SUFFIX)(src_addr + ei * offset, ((DATA_TYPE *)V(vd + fi))[ei]);
+        }
+    }
+}
+
+void glue(glue(helper_vsxei, BITS), POSTFIX)(CPUState *env, uint32_t vd, uint32_t rs1, uint32_t vs2, uint32_t nf)
+{
+    target_ulong src_addr = env->gpr[rs1];
+    DATA_TYPE *offsets = (DATA_TYPE *)V(vs2);
+    const target_ulong dst_eew = env->vsew;
+
+    for (int ei = env->vstart; ei < env->vl; ++ei) {
+#ifdef MASKED
+        if (!(V(0)[ei >> 3] & (1 << (ei & 0x7)))) {
+            continue;
+        }
+#endif
+        for (int fi = 0; fi <= nf; ++fi) {
+            switch (dst_eew) {
+            case 0:
+                stb(src_addr + ((DATA_TYPE *)V(vs2 + fi))[ei], V(vd + fi)[ei]);
+                break;
+            case 16:
+                stw(src_addr + offsets[ei] + sizeof(DATA_TYPE) * fi, ((uint16_t *)V(vd + fi))[ei]);
+                break;
+            case 32:
+                stl(src_addr + offsets[ei] + sizeof(DATA_TYPE) * fi, ((uint32_t *)V(vd + fi))[ei]);
+                break;
+            case 64: 
+                stq(src_addr + offsets[ei] + sizeof(DATA_TYPE) * fi, ((uint64_t *)V(vd + fi))[ei]);
+                break;
+            default:
+                tlib_abortf("Unsupported EEW");
+                break;
+            }
+        }
+    }
+}
+
 #undef SHIFT
 #undef DATA_TYPE
 #undef BITS
